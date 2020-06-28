5586896c727d526eab3766dc26fd709bcf21f558
Forzoom <forzoom@163.com>
2020-06-28 12:33:30 +0800

    feat: 删除peer中的core-js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     presets: [
         [ "@babel/preset-env", {
-            useBuiltIns: "false",
-            corejs: 2
+            useBuiltIns: "usage",
+            corejs: 3
         } ],
         "@babel/preset-typescript"
     ],
